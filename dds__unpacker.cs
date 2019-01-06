69a6634cba4ca3a315475c86c9d1e0533f1fa357
Man Sun <ramfanderu@mail.ru>
2019-01-06 03:25:02 +0300

    Update dds__unpacker.cs

--- dds__unpacker.cs
+++ dds__unpacker.cs
@@ -10,32 +10,32 @@ sealed class big2obj
 		{
 				string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.big", SearchOption.AllDirectories) ; 
 
-				foreach ( var file in files ) // для каждой строки , содержащей имя файла.big , содержащейся в массиве строк 
+				foreach ( var file in files ) // РґР»СЏ РєР°Р¶РґРѕР№ СЃС‚СЂРѕРєРё , СЃРѕРґРµСЂР¶Р°С‰РµР№ РёРјСЏ С„Р°Р№Р»Р°.big , СЃРѕРґРµСЂР¶Р°С‰РµР№СЃСЏ РІ РјР°СЃСЃРёРІРµ СЃС‚СЂРѕРє 
 				{
-						int dds_counter = 0 ; // счётчик появления текстур в файле , используется в качестве постфикса к имени текстуры 
+						int dds_counter = 0 ; // СЃС‡С‘С‚С‡РёРє РїРѕСЏРІР»РµРЅРёСЏ С‚РµРєСЃС‚СѓСЂ РІ С„Р°Р№Р»Рµ , РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ РІ РєР°С‡РµСЃС‚РІРµ РїРѕСЃС‚С„РёРєСЃР° Рє РёРјРµРЅРё С‚РµРєСЃС‚СѓСЂС‹ 
 
-						byte[] array1d = File.ReadAllBytes(file) ; // читаем весь файл в массив байт
+						byte[] array1d = File.ReadAllBytes(file) ; // С‡РёС‚Р°РµРј РІРµСЃСЊ С„Р°Р№Р» РІ РјР°СЃСЃРёРІ Р±Р°Р№С‚
 
-						for ( int i = 0 ; i < array1d.Length - 42 ; i++ ) // вдоль всего файла , проходим каждый байт
+						for ( int i = 0 ; i < array1d.Length - 42 ; i++ ) // РІРґРѕР»СЊ РІСЃРµРіРѕ С„Р°Р№Р»Р° , РїСЂРѕС…РѕРґРёРј РєР°Р¶РґС‹Р№ Р±Р°Р№С‚
 						{
-							//если нашли "сигнатуру" texture и следущую за ней "сигнатуру" symlist , то это точно dds-файл 
-							//if ( array1d[i+0] == 0x44 & array1d[i+1] == 0x44 & array1d[i+2] == 0x53 & array1d[i+3] == 0x20 & array1d[i+4] == 0x7C ) // нашли DDS 20 7C // не всегда подходит
+							//РµСЃР»Рё РЅР°С€Р»Рё "СЃРёРіРЅР°С‚СѓСЂСѓ" texture Рё СЃР»РµРґСѓС‰СѓСЋ Р·Р° РЅРµР№ "СЃРёРіРЅР°С‚СѓСЂСѓ" symlist , С‚Рѕ СЌС‚Рѕ С‚РѕС‡РЅРѕ dds-С„Р°Р№Р» 
+							//if ( array1d[i+0] == 0x44 & array1d[i+1] == 0x44 & array1d[i+2] == 0x53 & array1d[i+3] == 0x20 & array1d[i+4] == 0x7C ) // РЅР°С€Р»Рё DDS 20 7C // РЅРµ РІСЃРµРіРґР° РїРѕРґС…РѕРґРёС‚
 								if ( array1d[i+0] == 0x74 & array1d[i+1] == 0x65 & array1d[i+2] == 0x78 & array1d[i+3] == 0x74 & // text
 										 array1d[i+4] == 0x75 & array1d[i+5] == 0x72 & array1d[i+6] == 0x65 & array1d[i+7] == 0x00 & // ure?
 										 array1d[i+20] == 0x73 & array1d[i+21] == 0x79 & array1d[i+22] == 0x6D & array1d[i+23] == 0x6C & // syml
 										 array1d[i+24] == 0x69 & array1d[i+25] == 0x73 & array1d[i+26] == 0x74 & array1d[i+27] == 0x00 ) // ist?
 								{
-										byte[] ddsSize_bytes = { array1d[i+40+0] ,	array1d[i+40+1] ,	array1d[i+40+2] ,	array1d[i+40+3] } ;	// прочитали байты хранящие размер dds файла 
-										int ddsSize = BitConverter.ToInt32 ( ddsSize_bytes , 0 ) ; // = размер dds файла в байтах в чисовом виде // иногда здесь нужен long
+										byte[] ddsSize_bytes = { array1d[i+40+0] ,	array1d[i+40+1] ,	array1d[i+40+2] ,	array1d[i+40+3] } ;	// РїСЂРѕС‡РёС‚Р°Р»Рё Р±Р°Р№С‚С‹ С…СЂР°РЅСЏС‰РёРµ СЂР°Р·РјРµСЂ dds С„Р°Р№Р»Р° 
+										int ddsSize = BitConverter.ToInt32 ( ddsSize_bytes , 0 ) ; // = СЂР°Р·РјРµСЂ dds С„Р°Р№Р»Р° РІ Р±Р°Р№С‚Р°С… РІ С‡РёСЃРѕРІРѕРј РІРёРґРµ // РёРЅРѕРіРґР° Р·РґРµСЃСЊ РЅСѓР¶РµРЅ long
 
-										byte[] ddsArray = array1d.Skip(i+44).Take(ddsSize).Skip(0).Take(array1d.Length).SkipWhile(x =>(x==0xFF)).ToArray(); // кому надо - поймёт :D
+										byte[] ddsArray = array1d.Skip(i+44).Take(ddsSize).Skip(0).Take(array1d.Length).SkipWhile(x =>(x==0xFF)).ToArray(); // РєРѕРјСѓ РЅР°РґРѕ - РїРѕР№РјС‘С‚ :D
 
-										string ddsWritePath = file + dds_counter + ".dds" ; // создаём имя dds файла 
-																// в виде filename.big1.dds
+										string ddsWritePath = file + dds_counter + ".dds" ; // СЃРѕР·РґР°С‘Рј РёРјСЏ dds С„Р°Р№Р»Р° 
+																// РІ РІРёРґРµ filename.big1.dds
 
-										File.WriteAllBytes( ddsWritePath , ddsArray ) ; // записываем все байты из массива в файл
+										File.WriteAllBytes( ddsWritePath , ddsArray ) ; // Р·Р°РїРёСЃС‹РІР°РµРј РІСЃРµ Р±Р°Р№С‚С‹ РёР· РјР°СЃСЃРёРІР° РІ С„Р°Р№Р»
 
-										dds_counter++ ; // увеличиваем префикс имени файла
+										dds_counter++ ; // СѓРІРµР»РёС‡РёРІР°РµРј РїСЂРµС„РёРєСЃ РёРјРµРЅРё С„Р°Р№Р»Р°
 
 								} // if 
 
